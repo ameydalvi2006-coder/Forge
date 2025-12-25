725c647b04507350840bcb154b5b4fdf87f06b26
Amey Dalvi <ameydalvi2006@gmail.com>
2025-12-25 15:47:31 +0530

    Updated Import in Main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 import App from "./App";
-import { store } from "./app/Store";
+import { store } from "./app/store";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
